a613fd6654adbe45fbc485c71360f8734509cfb7
vitali <v.jalbu31@gmail.com>
2025-05-30 22:41:56 +0200

    clean

--- src/lib/query/use-delete-many.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { deleteMany } from "@/lib/api/strapi";
-
-export const useDeleteMany = () => {
-  return useMutation(({ resource, ids }) => deleteMany({ resource, ids }));
-};
